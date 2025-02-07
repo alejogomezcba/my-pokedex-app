160732789dd5b05b0f27dd9a885f9f83a37f2ca5
Alejo Gomez <alejogomez@nativodev.com>
2025-02-07 20:20:47 -0300

    update branch

--- src/components/PokemonNotfound/PokemonNotfound.tsx
+++ src/components/PokemonNotfound/PokemonNotfound.tsx
@@ -1,7 +1,7 @@
 import PokemonNotfoundImg from "../../assets/error404.png"
 import notFoundImg from "../../assets/noDataFound.png"
 
-import styles from './PokemonNotFound.module.css';
+import styles from './PokemonNotfound.module.css';
 import Footer from "../Footer";
 
 const PokemonNotfound = () => {

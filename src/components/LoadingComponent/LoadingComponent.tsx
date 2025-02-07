e22b3729de650e3f118f5c36415067206f689c84
Alejo Gomez <alejogomez@nativodev.com>
2025-02-07 14:58:40 -0300

    subiendo componente loading

---
+++ src/components/LoadingComponent/LoadingComponent.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import styles from './LoadingComponent.module.css';
+import Pokedex from "../../assets/pokedex.png";
+import PikachuImg from "../../assets/pikachu.gif";
+
+const LoadingComponent = () => {
+  const [text, setText] = useState("");
+  const fullText = "Loading...";
+  const typingSpeed = 150;
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullText.slice(0, index));
+      index++;
+      if (index > fullText.length) index = 0;
+    }, typingSpeed);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.pepito}>
+        <img className={styles.gameboyImage} src={Pokedex} alt="Pokedex" />
+        
+        {/* Contenedor de ambas pantallas */}
+        <div className={styles.screensContainer}>
+          {/* Pantalla Superior con Pikachu */}
+          <div className={styles.screenTop}>
+            <img className={styles.pikachuImg} src={PikachuImg} alt="Pikachu" />
+          </div>
+
+          {/* Pantalla Inferior con "Loading..." */}
+          <div className={styles.screenBottom}>
+            <p className={styles.loadingText}>{text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingComponent;

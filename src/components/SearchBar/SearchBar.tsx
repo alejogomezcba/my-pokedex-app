e931483297a5dccc27c04c3285a367f8fb098657
Alejo Gomez <alejogomez@nativodev.com>
2025-02-10 11:54:18 -0300

    update typescript errors

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,32 @@
 import styles from './SearchBar.module.css';
-
 import PokeballImg from '../../assets/pokeball.png';
 
-const SearchBar = ({ setItemToSearch, itemToSearch, navigate }) => {
-  return (
-    <header className={styles.header} >
-       <button onClick={() => navigate('/pokemons')} className={styles.pokeballButton}>
-         <img src={PokeballImg} alt="Pokeball" className={styles.pokeballImg} />
-         Go Back
-       </button>   
-       
-       <input
-        className={styles.input}
-        type="text"
-        placeholder='Search an Item'
-        value={itemToSearch}
-        onChange={(e) => setItemToSearch(e.target.value)}
-       />  
-    </header>
-  )
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface SearchBarProps {
+  setItemToSearch: (value: string) => void;
+  itemToSearch: string;
+  navigate: ReturnType<typeof useNavigate>;
 }
 
-export default SearchBar
+const SearchBar: FC<SearchBarProps> = ({ setItemToSearch, itemToSearch, navigate }) => {
+  return (
+    <header className={styles.header}>
+      <button onClick={() => navigate('/pokemons')} className={styles.pokeballButton}>
+        <img src={PokeballImg} alt="Pokeball" className={styles.pokeballImg} />
+        Go Back
+      </button>
+
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Search an Item"
+        value={itemToSearch}
+        onChange={(e) => setItemToSearch(e.target.value)}
+      />
+    </header>
+  );
+};
+
+export default SearchBar;
